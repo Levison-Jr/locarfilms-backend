a4b23c19f64007376f32316adef88ba2b49adc7e
levisonjr <levisonjr21@gmail.com>
2024-11-11 22:06:23 -0300

    Removendo 'required' dos compos do DTO de update de users

--- Dtos/Request/UpdateUserDto.cs
+++ Dtos/Request/UpdateUserDto.cs
@@ -5,13 +5,8 @@ namespace LocaFilms.Dtos.Request
 {
     public record UpdateUserDto
     {
-        [Required(ErrorMessage = "O campo 'FirstName' é obrigatório.")]
         public string? FirstName { get; set; }
-
-        [Required(ErrorMessage = "O campo 'LastName' é obrigatório.")]
         public string? LastName { get; set; }
-
-        [Required(ErrorMessage = "O campo 'PhoneNumber' é obrigatório.")]
         public string? PhoneNumber { get; set; }
     }
 }
