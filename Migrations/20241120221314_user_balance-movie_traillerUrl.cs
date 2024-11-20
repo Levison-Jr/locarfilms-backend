9cd8efa24876e47799ab1ab1b8ce1d20158ae32e
levisonjr <levisonjr21@gmail.com>
2024-11-20 19:16:44 -0300

    Migration user_balance-movie_traillerUrl

---
+++ Migrations/20241120221314_user_balance-movie_traillerUrl.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocaFilms.Migrations
+{
+    /// <inheritdoc />
+    public partial class user_balancemovie_traillerUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "YouTubeTraillerUrl",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "YouTubeTraillerUrl",
+                table: "Movies");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "AspNetUsers",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: true);
+        }
+    }
+}
