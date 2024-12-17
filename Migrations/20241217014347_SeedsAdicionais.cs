77ca2cbae9355735aef294049d6b70281cec5cc2
levisonjr <levisonjr21@gmail.com>
2024-12-16 23:49:55 -0300

    Add mais 3 filmes de seed

---
+++ Migrations/20241217014347_SeedsAdicionais.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LocaFilms.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedsAdicionais : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 12,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime", "YouTubeTraillerUrl" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "https://www.youtube.com/embed/H6A6GvryOG8" });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 13,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 14,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 16,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 20,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Category", "CostPerDay", "Description", "Duration", "ImageBannerUrl", "ImageIconUrl", "LastModifiedDateTime", "RegistrationDateTime", "ReleaseDate", "Status", "SubTitle", "Title", "YouTubeTraillerUrl" },
+                values: new object[,]
+                {
+                    { 21, "Ação", 7m, "Os Vingadores unem forças para enfrentar Thanos, que busca reunir todas as Joias do Infinito e destruir metade do universo.", "02:29", "https://image.tmdb.org/t/p/original/lmZFxXgJE3vgrciwuDib0N8CfQo.jpg", "https://image.tmdb.org/t/p/original/tstLvhcR281FF3b8iZs7vN2voE9.jpg", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateOnly(2018, 4, 26), 1, "Guerra Infinita", "Vingadores", "https://www.youtube.com/embed/htQch8jDZ4s" },
+                    { 22, "Animação", 5m, "As emoções de Riley enfrentam novos desafios ao surgirem sentimentos inesperados em sua adolescência.", "01:36", "https://image.tmdb.org/t/p/original/stKGOm8UyhuLPR9sZLjs5AkmncA.jpg", "https://image.tmdb.org/t/p/original/5w90NKW3pzurPMzNdzZmlto5Bt5.jpg", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateOnly(2024, 6, 13), 1, "", "Divertidamente 2", "https://www.youtube.com/embed/yAZxx8t9zig" },
+                    { 23, "Terror", 6m, "Após os eventos mortais em casa, a família Abbott agora enfrenta os terrores do mundo exterior em silêncio absoluto.", "01:37", "https://image.tmdb.org/t/p/original/i5IPrDQ8VevRKJPpLdmyvSLqtbr.jpg", "https://image.tmdb.org/t/p/original/sudgADOF0REnpM1GnU6DgSHV0QZ.jpg", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateOnly(2021, 5, 28), 1, "", "Um Lugar Silencioso 2", "https://www.youtube.com/embed/uGtoEvEjuEk" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 12,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime", "YouTubeTraillerUrl" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), "https://www.youtube.com/embed/FR9iLjOubWc" });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 13,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 14,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 16,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 20,
+                columns: new[] { "LastModifiedDateTime", "RegistrationDateTime" },
+                values: new object[] { new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 21, 0, 0, 500, DateTimeKind.Unspecified) });
+        }
+    }
+}
