0e68f7d4ccacec7c6352579df7573bcf45d4ac6d
levisonjr <levisonjr21@gmail.com>
2025-01-11 21:38:50 -0300

    feat: Movendo o Identity Services para um metodo de extensao proprio: AddIdentityServices.

--- Extensions/IdentitySetupExtensions.cs
+++ Extensions/IdentitySetupExtensions.cs
@@ -1,11 +1,87 @@
-﻿using LocaFilms.Models;
+﻿using System.Text;
+using LocaFilms.Contexts;
+using LocaFilms.Models;
+using LocaFilms.Services.Identity.Configurations;
 using LocaFilms.Services.Identity.Constants;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LocaFilms.Extensions
 {
     public static class IdentitySetupExtensions
     {
+        public static void AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDefaultIdentity<UserModel>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders()
+                .AddErrorDescriber<CustomIdentityErrorDescriber>();
+
+            var securityKeySettings = configuration.GetSection("JwtOptions:SecurityKey").Value;
+
+            if (string.IsNullOrEmpty(securityKeySettings))
+            {
+                throw new SecurityTokenException("JWT Security Key desconfigurada.");
+            }
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKeySettings));
+
+            services.Configure<JwtOptions>(options =>
+            {
+                configuration.GetSection(nameof(JwtOptions)).Bind(options);
+                options.SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration.GetSection("JwtOptions:Issuer").Value,
+
+                    ValidateAudience = true,
+                    ValidAudience = configuration.GetSection("JwtOptions:Audience").Value,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = securityKey,
+
+                    RequireExpirationTime = true,
+                    ValidateLifetime = true,
+
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(Policies.isEmployee, policy => 
+                    policy.RequireRole(Roles.Admin, Roles.Employee));
+            });
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // User settings.
+                options.User.RequireUniqueEmail = true;
+            });
+        }
         public static async Task CreateRolesAsync(this WebApplication app)
         {
             using (var scope = app.Services.CreateScope())
--- Program.cs
+++ Program.cs
@@ -1,13 +1,4 @@
-using LocaFilms.Contexts;
 using LocaFilms.Extensions;
-using LocaFilms.Models;
-using LocaFilms.Services.Identity.Configurations;
-using LocaFilms.Services.Identity.Constants;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace LocaFilms
 {
@@ -33,81 +24,7 @@ namespace LocaFilms
             builder.Services.AddSwaggerGen();
 
             builder.Services.AddApplicationServices(builder.Configuration);
-
-            builder.Services.AddDefaultIdentity<UserModel>()
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<AppDbContext>()
-                .AddDefaultTokenProviders()
-                .AddErrorDescriber<CustomIdentityErrorDescriber>();
-
-            var securityKeySettings = builder.Configuration.GetSection("JwtOptions:SecurityKey").Value;
-            var jwtOptionsSettings = builder.Configuration.GetSection(nameof(JwtOptions));
-
-            if (string.IsNullOrEmpty(securityKeySettings))
-            {
-                throw new SecurityTokenException("A JWT Security Key n�o est� configurada.");
-            }
-
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKeySettings));
-
-            builder.Services.Configure<JwtOptions>(options =>
-            {
-                options.Issuer = jwtOptionsSettings[nameof(JwtOptions.Issuer)] ?? "invalidIssuer";
-                options.Audience = jwtOptionsSettings[nameof(JwtOptions.Audience)] ?? "invalidAudience";
-                options.AccessTokenExpiration = int.Parse(jwtOptionsSettings[nameof(JwtOptions.AccessTokenExpiration)] ?? "0");
-                options.RefreshTokenExpiration = int.Parse(jwtOptionsSettings[nameof(JwtOptions.RefreshTokenExpiration)] ?? "0");
-                options.SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
-            });
-
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.RequireHttpsMetadata = false;
-
-                options.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidateIssuer = true,
-                    ValidIssuer = builder.Configuration.GetSection("JwtOptions:Issuer").Value,
-
-                    ValidateAudience = true,
-                    ValidAudience = builder.Configuration.GetSection("JwtOptions:Audience").Value,
-
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = securityKey,
-
-                    RequireExpirationTime = true,
-                    ValidateLifetime = true,
-
-                    // Distor��o/Atraso do timer/rel�gio utilizado na valida��o de hor�rios
-                    // Ex.: Valida��o do tempo de expira��o de tokens
-                    ClockSkew = TimeSpan.Zero
-                };
-            });
-
-            builder.Services.AddAuthorization(options =>
-            {
-                options.AddPolicy(Policies.isEmployee, policy => 
-                    policy.RequireRole(Roles.Admin, Roles.Employee));
-            });
-
-            builder.Services.Configure<IdentityOptions>(options =>
-            {
-                // Password settings.
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = true;
-                options.Password.RequiredLength = 6;
-                options.Password.RequiredUniqueChars = 1;
-
-                // User settings.
-                options.User.RequireUniqueEmail = true;
-            });
+            builder.Services.AddIdentityServices(builder.Configuration);
 
             var app = builder.Build();
 
