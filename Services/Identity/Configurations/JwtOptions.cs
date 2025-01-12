ee1aaabbbadc81207bfd6a24f3c1395a20bdcb4f
levisonjr <levisonjr21@gmail.com>
2025-01-11 21:36:38 -0300

    fix: modificador required nas propriedades

--- Services/Identity/Configurations/JwtOptions.cs
+++ Services/Identity/Configurations/JwtOptions.cs
@@ -2,12 +2,12 @@
 
 namespace LocaFilms.Services.Identity.Configurations
 {
-    public class JwtOptions
+    public class JwtOptions()
     {
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
+        public required string Issuer { get; set; }
+        public required string Audience { get; set; }
         public int AccessTokenExpiration { get; set; }
         public int RefreshTokenExpiration { get; set; }
-        public SigningCredentials SigningCredentials { get; set; }
+        public required SigningCredentials SigningCredentials { get; set; }
     }
 }
