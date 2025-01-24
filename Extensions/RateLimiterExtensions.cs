80d02fef2b9c82bac52a92fa1bec5a6945855847
levisonjr <levisonjr21@gmail.com>
2025-01-23 21:24:32 -0300

    feat: Feita configuracao do Rate Limiter

---
+++ Extensions/RateLimiterExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.Primitives;
+using System.Threading.RateLimiting;
+
+namespace LocaFilms.Extensions
+{
+    public static class RateLimiterExtensions
+    {
+        public static void AddRateLimiterServices(this IServiceCollection services)
+        {
+            services.AddRateLimiter(options =>
+            {
+                options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+                options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+                {
+                    var accessToken = httpContext.Features.Get<IAuthenticateResultFeature>()?
+                                .AuthenticateResult?.Properties?
+                                .GetTokenValue("access_token")?.ToString()
+                                ?? string.Empty;
+
+                    if (!StringValues.IsNullOrEmpty(accessToken))
+                    {
+                        return RateLimitPartition.GetFixedWindowLimiter(
+                            partitionKey: accessToken,
+                            factory: _ => new FixedWindowRateLimiterOptions
+                            {
+                                AutoReplenishment = true,
+                                PermitLimit = 500,
+                                QueueLimit = 0,
+                                Window = TimeSpan.FromMinutes(10)
+                            });
+                    }
+
+                    return RateLimitPartition.GetFixedWindowLimiter(
+                            partitionKey: "Anonnymous",
+                            factory: _ => new FixedWindowRateLimiterOptions
+                            {
+                                AutoReplenishment = true,
+                                PermitLimit = 200,
+                                QueueLimit = 0,
+                                Window = TimeSpan.FromMinutes(10)
+                            });
+                });                
+            });
+        }
+    }
+}
