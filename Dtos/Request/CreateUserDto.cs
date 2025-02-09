45d64711c62ad9e13b308ba374f48b66c60577f5
levisonjr <levisonjr21@gmail.com>
2025-02-09 15:24:17 -0300

    fix: Corrigida a regex para aceitar mais símbolos

--- Dtos/Request/CreateUserDto.cs
+++ Dtos/Request/CreateUserDto.cs
@@ -1,5 +1,4 @@
-﻿using LocaFilms.Enums;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace LocaFilms.Dtos.Request
 {
@@ -12,7 +11,7 @@ namespace LocaFilms.Dtos.Request
 
         [Required(ErrorMessage = "O campo 'Password' é obrigatório.")]
         [StringLength(30, MinimumLength = 6, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9-._@+]+$", ErrorMessage = $"A senha deve conter letra maiúscula, minúscula e número.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9._@!#$%&+-]{5,20}$", ErrorMessage = $"A senha deve conter letra maiúscula, minúscula e número. Entre 5 e 20 caracteres. Símbolos permitidos: ._@!#$%&+-")]
         public string? Password { get; set; }
 
         [Compare(nameof(Password), ErrorMessage = $"Os campos {nameof(Password)} e {nameof(PasswordConfirm)} devem ser iguais.")]
