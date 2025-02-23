78dfc59c7a9e74b9efbcfdd74e3dc4d3ac3fa6a8
levisonjr <levisonjr21@gmail.com>
2025-02-23 17:40:41 -0300

    test: Setup inicial

---
+++ tests/LocaFilms.Tests/Services/MovieServiceTests.cs
@@ -0,0 +1,11 @@
+﻿namespace LocaFilms.Tests.Services
+{
+    public class MovieServiceTests
+    {
+        [Fact]
+        public void GetAllMoviesAsync_ShouldReturnAllMovies()
+        {
+
+        }
+    }
+}
